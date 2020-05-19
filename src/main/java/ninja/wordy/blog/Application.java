5c7c937304250579012231fa293ccbe4f1ada151
Sam <ruwesam@gmail.com>
2020-05-19 10:16:50 -0400

    update

--- src/main/java/ninja/wordy/blog/Application.java
+++ src/main/java/ninja/wordy/blog/Application.java
@@ -2,11 +2,6 @@ package ninja.wordy.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.web.context.ConfigurableEmbeddedServletContainer;
-//import org.springframework.boot.web.context.context.embedded.EmbeddedServletContainerCustomizer;
-//import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
-//import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-//import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application {
@@ -15,21 +10,3 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
-
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
-
-///* TODO: uncomment to disable httponly cookies */
-//    @Override
-//    public void customize(final ConfigurableEmbeddedServletContainer container) {
-//        ((TomcatEmbeddedServletContainerFactory) container).addContextCustomizers(new TomcatContextCustomizer() {
-//            @Override
-//            public void customize(Context context) {
-//                context.setUseHttpOnly(false);
-//            }
-//        });
-//    }
-
-//}
